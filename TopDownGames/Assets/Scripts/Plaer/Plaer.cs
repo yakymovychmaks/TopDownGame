0106fd16d3f85b9eeefd8f3624f8a83ecf6997a6
Proces <pocerfeus@gmail.com>
2023-01-24 20:58:45 +0000

    I'm Create a hits
    I'm create a hits but I have a problem with collider and must create a enemy

--- TopDownGames/Assets/Scripts/Plaer/Plaer.cs
+++ TopDownGames/Assets/Scripts/Plaer/Plaer.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Plaer : MonoBehaviour
 {
+    [SerializeField] Bullet hits;
     [SerializeField] private int _whichPlaer = 0;
 
     public int selectPlaer => _whichPlaer;
 
-    
+    [SerializeField] private float Health;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Bullet"))
+        Destroy(gameObject);
+    }
+    // void Update()
+    // {
+
+    // }
 }
